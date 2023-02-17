b9d3c6796a503e6aa9561544ef495eb8cb02248d
Stael Edson <edsonstael@gmail.com>
2023-02-17 08:42:26 -0300

    requisito 4

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -25,6 +25,11 @@ const insertProducts = async (req, res) => {
   try {
     const { name } = req.body;
     console.log('insert Controllers: ', name);
+    if (!name) return res.status(400).json({ message: '"name" is required' });
+    if (name.length < 5) {
+      return res.status(422)
+        .json({ message: '"name" length must be at least 5 characters long' });
+    }
     const retorno = await productsServices.insertProducts(name);
     return res.status(201).json({ ...retorno });
   } catch (error) {
